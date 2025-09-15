e507c1934fd65dbe05aad796defdf9787da74c7e
user <cancrll1305@gmail.com>
2025-09-15 14:03:07 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -5,13 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.FragmentContainerView
 
-interface CoffeeListener {
-    fun onSelected(id: Int)
-}
-
-class MainActivity : AppCompatActivity(), CoffeeListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,22 +18,5 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Tambahkan ListFragment pertama kali (saat activity baru dibuat)
-        if (savedInstanceState == null) {
-            val listFragment = ListFragment()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, listFragment)
-                .commit()
-        }
-    }
-
-    override fun onSelected(id: Int) {
-        // Ganti ListFragment dengan DetailFragment secara dinamis
-        val detailFragment = DetailFragment.newInstance(id)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, detailFragment)
-            .addToBackStack(null) // supaya bisa back ke ListFragment
-            .commit()
     }
 }
